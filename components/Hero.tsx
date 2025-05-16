34cb4cc3a73cf0d464545e5f269656012baa5666
Karen Yao <karenyaooo@gmail.com>
2025-05-16 16:36:53 -0700

    feat(ui): added favicon and fixed navbar position

---
+++ components/Hero.tsx
@@ -0,0 +1,9 @@
+import styles from './Hero.module.css';
+import NavBar from './NavBar';
+
+export default function Hero() { 
+    return ( 
+        <div className={styles.banner}>
+        Hello!!</div>
+    );
+}
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,16 @@
 import styles from './NavBar.module.css';
 
+
 export default function NavBar() {
   return (
     <nav className={styles.nav} aria-label="Main navigation">
-      <div className={styles.title}>KY</div>
+      <div className={styles.title}>
+        <a href="#home">
+          <img src="/KY_Logo.png" alt="Karen Yao Logo" className={styles.logo} />
+        </a>
+      </div>
       <div className={styles.links}>
+        <a href="#about">About</a>
         <a href="#experience">Experience</a>
         <a href="#projects">Projects</a>
         <a href="#contact">Contact</a>
