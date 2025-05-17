0d4478b07543df4dab9071d6897ab63d32f6144b
Karen Yao <karenyaooo@gmail.com>
2025-05-16 19:45:09 -0700

    feat(about): updated About section

--- components/About.tsx
+++ components/About.tsx
@@ -3,9 +3,15 @@ import styles from './styles/About.module.css';
 export default function About() {
   return ( 
     <div className={styles.aboutMeContainer}>
-      <h2 className={styles.aboutMeHeader}>Driven by passion, caffeine, Stack Overflow, and blind faith </h2>
+      <h2 className={styles.aboutMeHeader}>Driven by passion, caffeine, Stack Overflow, and a Rubber Duck 🐥 </h2>
       <div className={styles.aboutMeContent}> 
         Hello! I am currently a student at Simon Fraser University, pursuing a degree in Computing Science with a minor in Statistics!
+
+        I am passionate about building impactful software and solving real-world problems. I love to learn about new technology and want to apply my skills to solving any issues I encounter, even if they are problems I create myself.
+
+        <br/><br/>
+        I love getting involved in the community, and I am currently the Vice President of the SFU Computing Science Student Society (CSSS). I love being able to meet new people and help around whenever I can. Outside of my busy schedule, I love to play video games, especially Valorant and any indie horror games, and also go cafe hopping around Vancouver!
+
       </div>
     </div>
   );
