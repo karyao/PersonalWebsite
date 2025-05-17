9d5a447322bbe5620cb7ae282f37da35f81bf457
Karen Yao <karenyaooo@gmail.com>
2025-05-16 21:39:55 -0700

    feat(scroll bar): created and customized scroll bar

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,12 @@
 import styles from './styles/Footer.module.css';
 
 export default function Footer() {
-    return (
-        <footer className={styles.footer}>
-            <div className={styles.socials}> 
-                <h3>Contact</h3>
-                <a href="https://www.linkedin.com/in/karenjhyao/">LinkedIn</a>
-            </div>
-            Made with 🍵 and a lot more ☕️  
-        </footer>
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.socials}> 
 
-    )
+      </div>
+      Made with 🍵 and a lot more ☕️  
+    </footer>
+  );
 }
