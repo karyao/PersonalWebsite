53ad3a5abaea7da04e928567173e5884b08a20e4
Karen Yao <karenyaooo@gmail.com>
2025-05-17 13:18:43 -0700

    refactor(Abotu): migrated stylign from CSS module to tailwind css

--- components/About.tsx
+++ components/About.tsx
@@ -1,17 +1,16 @@
-import styles from './About.module.css';
-
 export default function About() {
   return ( 
-    <div className={styles.aboutMeContainer}>
-      <h2 className={styles.aboutMeHeader}>Driven by Passion, Caffeine, Stack Overflow, and a Rubber Duck 🐥 </h2>
-      <div className={styles.aboutMeContent}> 
+    <div className="m6 text-black pl-20 pr-20">
+      <h2 className="font-mono text-3xl text-center p-5">
+        Driven by Passion, Caffeine, Stack Overflow, and a Rubber Duck 🐥 
+      </h2>
+      <div className="text-center"> 
         Hello! I am currently a student at Simon Fraser University, pursuing a degree in Computing Science with a minor in Statistics!
 
         I am passionate about building impactful software and solving real-world problems. I love to learn about new technology and want to apply my skills to solving any issues I encounter, even if they are problems I create myself.
 
         <br/><br/>
         I love getting involved in the community, and I am currently the Vice President of the SFU Computing Science Student Society. I love being able to meet new people and help around whenever I can. Outside of my busy schedule, I love to play video games, especially Valorant and any indie horror games, and also go cafe hopping around Vancouver!
-
       </div>
     </div>
   );
